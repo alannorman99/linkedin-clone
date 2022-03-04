ab4c345dbf2c1a1fa961fbca277ce195fe61d7cc
Alan Norman <asnorman3604@eagle.fgcu.edu>
2022-03-04 16:12:20 -0500

    Finished sidebar but can't fix bug where it stays in the middle of the screen.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,11 +3,20 @@ import React from 'react'
 import "./Sidebar.css"
 
 function Sidebar() {
+
+	const recentItem = (topic) => (
+		<div className="sidebar_recentItem">
+			<span className='sidebar_hash'>
+				#</span>
+			<p>{topic}</p>
+		</div>
+	);
+
 	return (
 		<div className="sidebar">
 
 			<div className="sidebar_top">
-				<img src="" alt="" />
+				<img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlip=rp-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JILWZIZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80" alt="" />
 				<Avatar className="sidebar_avatar" />
 				<h2>Alan Norman</h2>
 				<h4>alannorman00@gmail.com</h4>
@@ -24,8 +33,13 @@ function Sidebar() {
 				</div>
 			</div>
 
-			<div className='sidebar_button'>
+			<div className='sidebar_bottom'>
 				<p>Recent</p>
+				{recentItem("reactjs")}
+				{recentItem("programming")}
+				{recentItem("softwareengineering")}
+				{recentItem("design")}
+				{recentItem("developer")}
 			</div>
 		</div>
 	)
